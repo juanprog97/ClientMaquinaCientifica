658393fc6bf6ed2fcd2c410f078fa56613d8d2a1
juanprog97 <juancard190@gmail.com>
2021-12-16 21:30:55 -0500

    Fix mounted

--- src/components/UnityGame.vue
+++ src/components/UnityGame.vue
@@ -57,7 +57,7 @@ export default {
       );
     },
   },
-  activated() {
+  mounted() {
     this.$nextTick(() => {
       var eventTokenRefresh = document.addEventListener("changeToken", () => {
         this.refreshToken();
